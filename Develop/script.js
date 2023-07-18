ff2661ec7bf5c13714262e4c18fa9171bb363f58
Madeline K <madkovanda@gmail.com>
2023-07-17 23:39:36 -0600

    Updated array to correctly show charcters

--- Develop/script.js
+++ Develop/script.js
@@ -2,12 +2,12 @@
 var generateBtn = document.querySelector("#generate");
 //list of options
 // next part needs arrays for each section and each section is a variable?
-
-var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-var specialCharacters = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"]
-
+const characters = {
+upperCase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+lowerCase: "abcdefghijklmnopqrstuvwxyz",
+numbers : "0123456789",
+specialCharacters: "!@#$%^&*()?",
+}
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword(); 
@@ -33,6 +33,7 @@ var upperCase = confirm("Please click OK to include uppercase characters in your
 function upperCase () {
   if (upperCase === true) {
     //so if it is true need to store answer and add to password eventually
+
   }
 }
 
@@ -47,7 +48,9 @@ var specialCharacters = confirm("Please click OK to include Special Characters i
 
  
 }
-
+//for (var = i = 0; i <= passwordLength; i++) +{
+  //var rando
+//}
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
