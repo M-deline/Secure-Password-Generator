c44aee13c16aaeab1a4d4b29ad5d00a926c6883a
Madeline K <madkovanda@gmail.com>
2023-07-18 14:49:06 -0600

    learned alert method and changed display to alert that way it properly alerts the user that they are missing additional information or need to redo question

--- Develop/script.js
+++ Develop/script.js
@@ -14,19 +14,21 @@ lowerCase: "abcdefghijklmnopqrstuvwxyz",
 numbers : "0123456789",
 specialCharacters: "!@#$%^&*()?",
 };
-// Add event listener to generate button, not working
+
 generateBtn.addEventListener("click", writePassword);
 
-//says illegal return on console 
+//says illegal return on console
+
+// ALERT NEED TO CREDIT https://www.w3schools.com/jsref/met_win_alert.asp
 var userAnswer = (prompt("How many characters would like in your password?"));
 if (!userAnswer) {
-  display("Try again");
+  alert("Try again");
 }
 else if (userAnswer < 8) {
-display("Password length must be 8 or more characters!");
+alert("Password length must be 8 or more characters!");
 }
 else if (userAnswer > 129) {
-display("Password length must be less than 129 characters!");
+alert("Password length must be less than 129 characters!");
 }
 else {
   confirmUpperCase = confirm("Please click OK to include uppercase characters in your password.")
@@ -36,9 +38,9 @@ else {
 }
 //but what if the user says no to everything?
 if (!confirmUpperCase && !confirmLowerCase && !confirmNumbers && !confirmSpecialCharacters) {
-  display("you must pick some criterias")
+  alert("you must pick some criterias")
 }
-//if (confirm)
+if (confirm)
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword(); 
@@ -61,23 +63,20 @@ function writePassword() {
 for (var i = 0; i < password.length; i++) {
   password += generatePassword.charAt(Math.floor(Math.random() * characters.length));
 };
-// so password is generated password
-// this is a local filllelellel password = generatePassword[index];
-
 
 
 
 // GIVEN I need a new, secure password
-// WHEN I click the button to generate a password
-// THEN I am presented with a series of prompts for password criteria
+// WHEN I click the button to generate a password yes
+// THEN I am presented with a series of prompts for password criteria yes
 // WHEN prompted for password criteria
-// THEN I select which criteria to include in the password
+// THEN I select which criteria to include in the password yes
 // WHEN prompted for the length of the password
-// THEN I choose a length of at least 8 characters and no more than 128 characters
+// THEN I choose a length of at least 8 characters and no more than 128 characters yes
 // WHEN asked for character types to include in the password
-// THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
+// THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters yes
 // WHEN I answer each prompt
-// THEN my input should be validated and at least one character type should be selected
+// THEN my input should be validated and at least one character type should be selected !!
 // WHEN all prompts are answered
 // THEN a password is generated that matches the selected criteria
 // WHEN the password is generated
