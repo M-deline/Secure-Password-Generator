2fda759510c5c2ee099b01ce1f8253c122c26b98
Madeline K <madkovanda@gmail.com>
2023-07-20 08:23:41 -0600

    redid array, trying to fix error codes so that it works

--- Develop/script.js
+++ Develop/script.js
@@ -1,17 +1,20 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-//I need to add the user input variables
-
-// var password = document.getElementById
-
 //array for options
+var userAnswer;
+const characters = {
+  upperCase: ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"],
+  lowerCase: ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
+  numbers: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+  specialCharacters: ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "?"],
+};
+  // var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+  // var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  // var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+  // var specialCharacters = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "?"];
+  // var characters;
+// characters = characters.concat;
 
-  var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-  var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-  var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-  var specialCharacters = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "?"];
-  
-// var array2 = characters.concat;
 var userAnswer;
 var confirmUpperCase;
 var confirmLowerCase;
@@ -24,23 +27,8 @@ generateBtn.addEventListener("click", function writePassword(){
 });
 
 // ALERT NEED TO CREDIT https://www.w3schools.com/jsref/met_win_alert.asp
-function writePassword() {
-  var password = generatePassword(); 
-  var passwordText = document.querySelector("#password");
-if (password.upperCase === true) {
-  password = password += upperCase; 
-}
-if (password.lowerCase === true) {
-  password = passwordText += lowerCase; 
-}
-if (password.numbers === true) {
-  password = passwordText += numbers; 
-}
-if (password.specialCharacters === true) {
-  password = passwordText += specialCharacters; 
-}
 
-
+// function writePassword() {
 var userAnswer = prompt("How many characters would like in your password?");
 if (!userAnswer) {
   alert("Try again");
@@ -61,9 +49,41 @@ else {
 if (!confirmUpperCase && !confirmLowerCase && !confirmNumbers && !confirmSpecialCharacters) {
   alert("you must pick some criterias")
 }
-if (confirm)
+//this is if they choose lowercase and uppercase
+else if (confirmLowerCase && confirmUpperCase && !confirmNumbers && !confirmNumbers)
+characters = characters.concat(lowerCase, upperCase);
+//if they choose uppercase & numbers 
+else if (confirmUpperCase && confirmNumbers && !confirmLowerCase && !confirmSpecialCharacters)
+characters = characters.concat(upperCase, numbers);
+//if they choose confirm uppercase and symbols
+else if (confirmUpperCase && confirmSpecialCharacters && !confirmLowerCase && !confirmNumbers)
+characters = characters.concat(upperCase, specialCharacters);
+// if they choose lowercase and numbers
+else if (confirmLowerCase && confirmNumbers && !confirmSpecialCharacters && !confirmUpperCase)
+characters = characters.concat(lowerCase, numbers);
+// if they choose lowercase and symbols
+else if (confirmLowerCase && confirmSpecialCharacters && !confirmNumbers && !confirmUpperCase)
+characters = characters.concat(lowerCase, specialCharacters);
+//if they choose everything 
+else if (confirmLowerCase && confirmLowerCase && confirmSpecialCharacters && confirmNumbers)
+characters = characters.concat(upperCase, lowerCase, numbers, specialCharacters);
 // Write password to the #password input
 
+  function writePassword() {
+    var password = generatePassword(); 
+    var passwordText = document.querySelector("#password");
+  if (password.upperCase === true) {
+    password = password += upperCase; 
+  }
+  if (password.lowerCase === true) {
+    password = passwordText += lowerCase; 
+  }
+  if (password.numbers === true) {
+    password = passwordText += numbers; 
+  }
+  if (password.specialCharacters === true) {
+    password = passwordText += specialCharacters; 
+  }
 
   var password = [];
 for (var i =0; i <= password.length; i++) {
@@ -75,11 +95,10 @@ userAnswer(ps);
 return ps;
 }
 
+
 //used this to help me with above: https://dev.to/code_mystery/random-password-generator-using-javascript-6a
 
-// so we do characters.length bcause thats the name of the array that holds the symbols/numbers/letters
 
-//this is incorrect
 
 
 // function document.getElementById("password).value") = password;("password");
