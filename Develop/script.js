9d503a4e8b49b5c8515dc6c7ec83e4970323b534
Madeline K <madkovanda@gmail.com>
2023-07-20 18:28:36 -0600

    updated javascript still coming up with true or false and not pw

--- Develop/script.js
+++ Develop/script.js
@@ -5,7 +5,7 @@ var generateBtn = document.querySelector("#generate");
   var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
   var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
   var specialCharacters = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "?"];
-
+var all = upperCase + lowerCase + numbers + specialCharacters;
 var password = "";
 
 
@@ -25,11 +25,12 @@ var userAnswer = prompt("How many characters would like in your password?");
 if (!userAnswer) {
   alert("Try again");
 }
-else if (userAnswer < 8) {
+else if (userAnswer < 7) {
 alert("Password length must be 8 or more characters!");
 }
 else if (userAnswer > 129) {
 alert("Password length must be less than 129 characters!");
+return
 }
 
 else {
@@ -39,22 +40,28 @@ else {
  var specialCharacters = confirm("Please click OK to include special characters in your password.");
 
 }
+if (all === confirm) {
+  password += all[Math.floor(Math.random() * all.length)];
+}
 if (upperCase === confirm) {
-  password + upperCase[Math.floor(Math.random() * upperCase.length)];
+  password += upperCase[Math.floor(Math.random() * upperCase.length)];
 }
 if (lowerCase === confirm) {
-  password + lowerCase[Math.floor(Math.random() * lowerCase.length)];
+  password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
 }
 if (numbers === confirm) {
-  password + numbers[Math.floor(Math.random() * numbers/length)];
+  password += numbers[Math.floor(Math.random() * numbers/length)];
 }
 if (specialCharacters === confirm) {
-  password + specialCharacters[Math.floor(Math.random() * specialCharacters.length)];
+  password += specialCharacters[Math.floor(Math.random() * specialCharacters.length)];
 }
 
 if (!confirm(upperCase) && !confirm(lowerCase) && !confirm(numbers) && !confirm(specialCharacters)) {
   alert("you must pick some criterias");
+  return
 }
+console.log(password);
+
 
   generateBtn.addEventListener("click", writePassword);{
    generatePassword();}
@@ -64,7 +71,6 @@ if (!confirm(upperCase) && !confirm(lowerCase) && !confirm(numbers) && !confirm(
     var passwordText = document.querySelector("#password");
     
     passwordText.value = password;
-    return password
    }
   }
 // }
